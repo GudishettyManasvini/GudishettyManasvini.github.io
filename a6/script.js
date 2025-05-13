571cf8695d19d522f9bf20894d781a39c97cd378
GudishettyManasvini <gudishetty.manasvini@gmail.com>
2025-05-13 12:46:49 +0530

    updated

--- a6/script.js
+++ a6/script.js
@@ -1,6 +1,9 @@
 function msg()
 {
-    var email=document.getElementById('email').value;
-    var pass=document.getElementById('pass').value;
-    document.getElementById('op').innerHTML="E-Mail is "+email+"<br> Password is :"+pass;
+   
+    let email=document.getElementById('email').value;
+    let pass=document.getElementById('pass').value;
+    document.getElementById('op').innerText=`Email: ${email},Password: ${pass}`;
+    console.log("Emai:", email);
+    console.log("Password:", pass);
 }
