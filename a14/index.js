642f813fbdab31839893b4e9b3c89f3e4d670cdf
GudishettyManasvini <gudishetty.manasvini@gmail.com>
2025-05-18 15:01:05 +0530

    updated

--- a14/index.js
+++ a14/index.js
@@ -1,3 +1,16 @@
-fetch("https://jsonplaceholder.typicode.com/posts").then((res)=>
-res.json().then((data)=>console.log(data))
-);
+function greet(){
+  console.log("Hello World")
+}
+ 
+async function dispData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json()
+  console.log(data);
+}
+async function main() {
+    await dispData();
+    greet();
+}
+dispData();
+greet();
+ 
